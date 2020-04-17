258c67df20ab22709fbd094863aac1bbb04ac2a4
Yriu2902VK <pchelovod2902@gmail.com>
2020-04-17 18:27:35 +0300

    внесение изменений

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
   </head>
   <body>
     <div class="container">
-      <div class="row coffee-machine">
+      <div class="row coffee-machine" id="coffee">
         <div class="col-6 coffee-list d-flex flex-column justify-content-around">
           <div class="coffee-item">
             <img src="img/americano.png" alt="">
@@ -64,6 +64,7 @@
       </div>
      </div>
     <!-- Optional JavaScript -->
+    <script src="script.js"></script>
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
