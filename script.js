9e90e6c48c13c92ee2e5ffb3d8230a9d679f193c
Yriu2902VK <pchelovod2902@gmail.com>
2020-04-20 18:11:35 +0300

    обновление файлов

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-"user script";
+"user strict";
+
+/*function buyCoffee(name, price, element){
+  console.log([name, price, element]);
+}
+
+let elem = document.querySelectorAll(".coffee-item");
+elem[1].onclick = function() {
+  buyCoffee('Американо', 50, this);
+}*/
+//--------------------------------Планирование-----------------------------------------
+// таймаут
+
+/*let timeout = setTimeout(paintBody , 5000, 'aqua');
+let changeButton = document.querySelector(".btn");
+changeButton.onclick = function() {
+  clearTimeout(timeout);
+}
+
+function paintBody(color) {
+  document.body.style.background = color;
+}*/
+/*setTimout(function() {
+  paintBody('aqua');
+}, 5000);*/
+/*setTimeout(paintBody , 5000);
+
+function paintBody() {
+  document.body.style.background = "crimson";*/
+
+/*let coffeeMachine = document.querySelector('.coffee-machine');
+coffeeMachine.style.position = "absolute";*/
+
+let interval = setInterval(trashConsole, 1000);
+
+let changeButton = document.querySelector(".btn");
+changeButton.onclick = function() {
+  clearInterval(interval);
+}
+
+function trashConsole() {
+  console.log(Math.random());
+}
+
+
+
+
+
